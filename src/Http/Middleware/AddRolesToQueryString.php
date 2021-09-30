116071dc7af0a7fd597521092e0b648ddb33c732
edalzell <erin@silentz.co>
2021-09-30 16:09:03 -0700

    refactor

--- src/Http/Middleware/AddRolesToQueryString.php
+++ src/Http/Middleware/AddRolesToQueryString.php
@@ -23,28 +23,40 @@ class AddRolesToQueryString
         // if it doesn't have roles, redirect to add roles to query string
         // @todo should check for get as well, don't want to add to non-page routes, like the "review" url
         // can we check the name of the route to see if it starts w/ `statamic`?
-        /* @var \Statamic\Auth\User */
-        if (! $user = $request->user()) {
-            if ($request->query('roles')) {
-                return redirect($request->fullUrlWithoutQuery('roles'));
-            }
+        $qsRoles = $request->query('roles');
 
+        /** @var \Statamic\Auth\User */
+        $user = $request->user();
+
+        $userRoles = $user?->roles()->keys()->all();
+
+        // not logged in, but there are roles, likely someone shared this link
+        if (! $user && $qsRoles) {
+            return redirect($request->fullUrlWithoutQuery('roles'));
+        }
+
+        // not logged, move on
+        if (! $user) {
             return $next($request);
         }
 
-        $roles = $user->roles()->keys()->all();
-        if (! $request->has('roles') && $roles) {
-            return redirect($request->fullUrlWithQuery(['roles' => $roles]));
+        // if no roles on qs and the user has roles, redirect w/ roles
+        if (! $qsRoles && $userRoles) {
+            return redirect($request->fullUrlWithQuery(['roles' => $userRoles]));
         }
 
-        if ($request->has('roles') && $roles) {
-            if ($request->get('roles') == $roles) {
+        // if there are roles on the qs and the user has roles
+        if ($qsRoles && $userRoles) {
+            // if the roles are the same, don't do anything
+            if (collect($qsRoles)->diff($userRoles)->isEmpty()) {
                 return $next($request);
             }
-            // remove existing roles params
-            $query = Arr::except($request->query(), 'roles');
 
-            return redirect($request->url().'?'.Arr::query(array_merge($query, ['roles' => $roles])));
+            // if the roles are different, remove existing roles and put user ones back on
+            return redirect($request->url().'?'.Arr::query(array_merge(
+                Arr::except($request->query(), 'roles'),
+                ['roles' => $userRoles]
+            )));
         }
 
         return $next($request);
