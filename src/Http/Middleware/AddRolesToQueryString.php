7efac3d95b9a659e2df9a206334e3fce1431d291
edalzell <erin@silentz.co>
2021-10-15 15:07:38 -0700

    remove method comment

--- src/Http/Middleware/AddRolesToQueryString.php
+++ src/Http/Middleware/AddRolesToQueryString.php
@@ -3,20 +3,14 @@
 namespace TransformStudios\Gated\Http\Middleware;
 
 use Closure;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Statamic\Facades\User as UserFacade;
 use Statamic\Support\Arr;
 
 class AddRolesToQueryString
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @return mixed
-     */
-    public function handle($request, Closure $next)
+    public function handle(Request $request, Closure $next)
     {
         if (! config('gated.enabled')) {
             return $next($request);
