b81482215be75e3b57ed95e8beb887b2dc0f9dfa
edalzell <erin@silentz.co>
2022-11-17 15:44:33 -0800

    add password check, refactor role check

--- src/Tags/Gate.php
+++ src/Tags/Gate.php
@@ -2,16 +2,19 @@
 
 namespace TransformStudios\Gated\Tags;
 
+use Statamic\Tags\Concerns\RendersForms;
 use Statamic\Tags\Tags;
 
 class Gate extends Tags
 {
+    use RendersForms;
+
     /**
      * @return bool
      */
     public function authorized()
     {
-        if (! $this->context->value('is_gated')) {
+        if ($this->context->raw('gated_by') !== 'roles') {
             return true;
         }
 
@@ -19,5 +22,22 @@ class Gate extends Tags
             request('roles', []),
             $this->context->raw('authorized_roles')
         ));
+    }
+
+    public function passwordForm()
+    {
+        $action = route('statamic.gated.password.store');
+        $method = 'POST';
+
+        $html = $this->formOpen($action, $method);
+
+        $html .= '<input type="hidden" name="redirect" value="'.request()->query('redirect').'" />';
+        $html .= '<input type="hidden" name="id" value="'.request()->query('id').'" />';
+
+        $html .= $this->parse();
+
+        $html .= $this->formClose();
+
+        return $html;
     }
 }
