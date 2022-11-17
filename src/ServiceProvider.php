6b01cac455b42b8564fbf854429effb57d7553d8
edalzell <erin@silentz.co>
2022-11-17 15:45:03 -0800

    rename

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -3,14 +3,14 @@
 namespace TransformStudios\Gated;
 
 use Statamic\Providers\AddonServiceProvider;
-use TransformStudios\Gated\Http\Middleware\AddRolesToQueryString;
+use TransformStudios\Gated\Http\Middleware\HandleGate;
 use TransformStudios\Gated\Tags\Gate;
 
 class ServiceProvider extends AddonServiceProvider
 {
     protected $middlewareGroups = [
         'web' => [
-            AddRolesToQueryString::class,
+            HandleGate::class,
         ],
     ];
 
